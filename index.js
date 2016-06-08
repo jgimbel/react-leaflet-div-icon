06a96aa3ab12e9e65e2ee37e1750c797a4dd5342
Joel Gimbel <jgimbel@live.com>
2016-06-08 14:03:25 -0500

    initial copy of react-leaflet's marker

---
+++ index.js
@@ -0,0 +1,49 @@
+import { PropTypes } from 'react';
+import { Icon, marker } from 'leaflet';
+
+import latlngType from './types/latlng';
+import MapLayer from './MapLayer';
+
+export default class Marker extends MapLayer {
+  static propTypes = {
+    icon: PropTypes.instanceOf(Icon),
+    opacity: PropTypes.number,
+    position: latlngType.isRequired,
+    zIndexOffset: PropTypes.number,
+  };
+
+  componentWillMount() {
+    super.componentWillMount();
+    const { map: _map, layerContainer: _lc, position, ...props } = this.props;
+    this.leafletElement = marker(position, props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.position !== prevProps.position) {
+      this.leafletElement.setLatLng(this.props.position);
+    }
+    if (this.props.icon !== prevProps.icon) {
+      this.leafletElement.setIcon(this.props.icon);
+    }
+    if (this.props.zIndexOffset !== prevProps.zIndexOffset) {
+      this.leafletElement.setZIndexOffset(this.props.zIndexOffset);
+    }
+    if (this.props.opacity !== prevProps.opacity) {
+      this.leafletElement.setOpacity(this.props.opacity);
+    }
+    if (this.props.draggable !== prevProps.draggable) {
+      if (this.props.draggable) {
+        this.leafletElement.dragging.enable();
+      }
+      else {
+        this.leafletElement.dragging.disable();
+      }
+    }
+  }
+
+  render() {
+    return this.renderChildrenWithProps({
+      popupContainer: this.leafletElement,
+    });
+  }
+}
